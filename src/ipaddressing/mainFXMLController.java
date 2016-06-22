d86243603aca5177429b0df9bb40ada439e927da
myotherself <myotherself@myotherself-G31M-ES2L>
2016-06-23 00:54:54 +0600

    valid ip checker

--- src/ipaddressing/mainFXMLController.java
+++ src/ipaddressing/mainFXMLController.java
@@ -6,17 +6,17 @@
 package ipaddressing;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -40,15 +40,15 @@ public class mainFXMLController implements Initializable {
     @FXML
     private TextField conBtnRoutersTF;
     @FXML
-    private TextField maxDevicesTF;
-    @FXML
     private AnchorPane rightPane;
     
     //javafx variables without SB
-    private Button partTwoBtn = new Button();
+    private Button partTwoBtn;
+    private Button partThreeBtn;
     private Label enterRoutersNumber;
     //private TextField[][] routersConTFs = new TextField[510][3];
     private TextField[] noOfLansTFs;
+    private TextField[][] noOfDevicesTFs;
     
     // logic's variables
     private double height;
@@ -62,29 +62,28 @@ public class mainFXMLController implements Initializable {
     private int conBtnRouters;
     //private int[][] routersGraph = new int[500][500]; // 0-> new index value, then other routers numbers those have connection with this router 
     private int[][] lansGraph;// 0-> how many lan in this router, 1,2,...,n -> number of devices in each lan
-    private int maxDevice;
+    private ArrayList<LAN> col;
+    private int hostPortionLen;
+    private String errorMsg;
+    @FXML
+    private Label errorMsgLbl;
         
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        col = new ArrayList<LAN>();
         noOfLansTFs = new TextField[510];
-        lansGraph = new int[500][500];
+        noOfDevicesTFs = new TextField[510][100];
+        lansGraph = new int[100][5];
         height = 1000;
         btnH = 70;
         tfH = 45;
         adjustHeight(height);
-        partTwoBtn = new Button("new Button");
-//        partTwoBtn.setOnAction((event) -> {
-//            // Button was clicked, do something...
-//            System.out.println("huhuhihihh");
-//        });
+        partTwoBtn = new Button("Next");
         partTwoBtn.setOnAction(this::partTwoBtnAction);
-//        partTwoBtn.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override public void handle(ActionEvent e) {
-//                leftVBox.getChildren().add(new Label("hellow"));
-//                System.out.println("klsdfjklsdjflsjfj");
-//            }
-//        });
+        partThreeBtn = new Button("Submit");
+        partThreeBtn.setOnAction(this::partThreeBtnAction);
+
     }    
 
     @FXML
@@ -92,37 +91,20 @@ public class mainFXMLController implements Initializable {
         ip = ipTF.getText();
         subnetLen = Integer.parseInt(cidrTF.getText());
         noOfRouters = Integer.parseInt(routersTF.getText());
-//        for(int i = 1; i <= noOfRouters; i++){
-//            routersGraph[i][0] = 1;
-//        }
         conBtnRouters = Integer.parseInt(conBtnRoutersTF.getText());
-        maxDevice = Integer.parseInt(maxDevicesTF.getText());
+        if(!validIP()){
+            errorMsgLbl.setText(errorMsg);
+        }
+        else errorMsgLbl.setText("OK!");
         addPartTwoElements();
     }
     
     private void addPartTwoElements(){
-        HBox hb = null;
-//        enterRoutersNumber = new Label("Enter Each Routers' Number in The Fields:");
-//        enterRoutersNumber.setPrefSize(Double.MAX_VALUE, tfH);
-//        partOneVBox.getChildren().add(enterRoutersNumber);
-//        for(int i = 1; i <= conBtnRouters; i++){
-//            routersConTFs[i][0] = new TextField();
-//            routersConTFs[i][1] = new TextField();
-//            routersConTFs[i][0].setPrefHeight(Double.MAX_VALUE);
-//            routersConTFs[i][1].setPrefHeight(Double.MAX_VALUE);
-//            Label lbl = new Label("  has connection with  ");
-//            lbl.setPrefHeight(Double.MAX_VALUE);
-//            hb = new HBox(routersConTFs[i][0],lbl , routersConTFs[i][1]);
-//            hb.setAlignment(Pos.CENTER);
-//            hb.setPrefSize(Double.MAX_VALUE, tfH);
-//            partOneVBox.getChildren().add(hb);
-//        }
-        
         Label lanNo = new Label("Number Of LAN(s) In Each Router:");
         lanNo.setPrefSize(Double.MAX_VALUE, tfH);
         leftVBox.getChildren().add(lanNo);
         for(int i = 1; i <= noOfRouters; i++){
-            noOfLansTFs[i] = new TextField("3");
+            noOfLansTFs[i] = new TextField("2");
             noOfLansTFs[i].setPrefHeight(tfH);
             //noOfLansTFs[i].setPrefWidth(Double.MAX_VALUE);
             Label lbl = new Label("Number Of LAN(s) In Router " + i + ":");
@@ -151,12 +133,94 @@ public class mainFXMLController implements Initializable {
     }
     
     private void addPartThreeElements(){
-         System.out.println("working");
-         System.out.println(noOfLansTFs[1].getText());
+         //System.out.println("working");
+         //System.out.println(noOfLansTFs[1].getText());
+         Label info = new Label("Information About The Number Of Devices");
+         info.setPrefHeight(tfH);
+         leftVBox.getChildren().add(info);
+         for(int i = 1; i <= noOfRouters; i++){
+             Label lbl = new Label("Router-" + i +"'s Information:");
+             lbl.setPrefHeight(tfH);
+             leftVBox.getChildren().add(lbl);
+             for(int j = 1; j <= lansGraph[i][0]; j++){
+                 Label lbl2 = new Label("Number Of Devices In LAN " + j + " of Router-" + i + ":");
+                 lbl2.setPrefHeight(tfH);
+                 noOfDevicesTFs[i][j] = new TextField("1");
+                 noOfDevicesTFs[i][j].setPrefHeight(tfH);
+                 leftVBox.getChildren().add(lbl2);
+                 leftVBox.getChildren().add(noOfDevicesTFs[i][j]);
+             }
+         }
+         partThreeBtn.setPrefSize(Double.MAX_VALUE,btnH);
+         leftVBox.getChildren().add(partThreeBtn);
+    }
+    private void partThreeBtnAction(ActionEvent ae){
+        for(int i = 1; i <= noOfRouters; i++){
+            for(int j = 1; j <= lansGraph[i][0]; j++){
+                int n = Integer.parseInt(noOfDevicesTFs[i][j].getText());
+                LAN lan = new LAN(i, j, n);
+                col.add(lan);
+            }
+        }
+        //Collections.sort(col,(la , lb)-> lb.getNoOfDevices() - la.getNoOfDevices());
+        Collections.sort(col,(la , lb)-> {
+            int difD = lb.getNoOfDevices() - la.getNoOfDevices();
+            int difR = la.getRoutersNo() - lb.getRoutersNo();
+            if(difD != 0)
+                return difD;
+            else if(difR != 0)
+                return 1;
+            else return la.getLANNo()- lb.getLANNo();
+        });
+        callAllTheFunctions();
     }
     
+    private void callAllTheFunctions(){
+        if(!validIP()){
+            errorMsgLbl.setText(errorMsg);
+        }
+        else errorMsgLbl.setText("OK!");
+    }
+    
+    private boolean validIP(){
+        StringTokenizer str = new StringTokenizer(ip, ".");
+        if(ip.length() > 15){
+            if(ip.length() > 35){
+                errorMsg = "Invalid IP Address - TOO BIG. SHOULD CONTAIN EXACTLY 8 DIGITS ON EACH BLOCK";
+                return false;
+            }
+            for(int i = 0; i < ip.length(); i++){
+                int x = ip.charAt(i) - '0';
+                if(x>1){
+                    errorMsg = "Invalid IP Address - ALl IP DIGITS SHOULD BE ONLY BINRAY OR ONLY DECIAML";
+                    return false;
+                }
+            }
+        }
+        else{
+            if(ip.length() < 7){
+                errorMsg = "Invalid IP Address - TOO SHORT. SHOULD CONTAIN ATLEAST ONE DIGIT ON EACH BLOCK";
+                return false;
+            }
+            while(str.hasMoreTokens()){
+                String tkn = str.nextToken().toString();
+                if(tkn.charAt(0) == '0' && (tkn.length() > 1)){
+                    errorMsg = "Invalid IP Address - LEADING ZERO(S). ALl IP DIGITS SHOULD BE ONLY BINRAY OR ONLY DECIAML";
+                    return false;
+                }
+            }
+            
+        }
+        str = new StringTokenizer(ip, ".");
+        if(str.countTokens() != 4){
+            //errorMsg = (" "+str.countTokens()+" : ");
+            errorMsg = "Invalid IP Address - TOO MANY/LESS BLOCKS. SHOULD BE 4";
+            return false;
+        }
+        return true;
+    } 
+    
     private void adjustHeight(double h){
-        //System.out.println("H;" + h);
         leftRoot.setMinHeight(h);
         leftVBox.setMinHeight(h);
     }
